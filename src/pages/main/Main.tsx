1fd5a42e64911af32c297bd9a9d6dc6431fa6ca0
eunjae <rladmswo1715@naver.com>
2024-11-10 15:50:07 +0900

    design: 레이아웃 수정 및 pr1 메인

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,5 +1,24 @@
 const Main = () => {
-  return <div>메인</div>;
+  return (
+    <div>
+      <h2>Pr1</h2>
+      <p>
+        <span>
+          클론 페이지 디자인 출처 :
+          <a
+            href="https://d.cafe24.com/sample?productCode=PTMD837550&frame=P"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://d.cafe24.com/sample?productCode=PTMD837550&frame=P
+          </a>
+        </span>
+        <br />
+        <br />
+        <a href="/pr1">첫번째 클론페이지 바로가기</a>
+      </p>
+    </div>
+  );
 };
 
 export default Main;
